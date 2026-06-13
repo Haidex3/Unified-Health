2b1644eb839e75a59bf250b0e87dfd24f9ab0390
Maldeojos <tiansanchez2000@gmail.com>
2026-06-13 16:23:59 -0400

    Se añadio dependencias de navegacion NavController e inyeccion HiltViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
 }
 
 android {
@@ -48,6 +51,13 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.navigation.compose)
+
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    ksp(libs.hilt.android.compiler)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- app/src/main/java/com/develop/myapplication/ui/navigation/NavController.kt
+++ app/src/main/java/com/develop/myapplication/ui/navigation/NavController.kt
@@ -1,2 +1,23 @@
 package com.develop.myapplication.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.develop.myapplication.ui.screens.HomeScreen
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Home
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+
+
+    NavHost(navController = navController, startDestination = Home){
+        composable<Home>{
+            HomeScreen(navController = navController)
+        }
+    }
+}
