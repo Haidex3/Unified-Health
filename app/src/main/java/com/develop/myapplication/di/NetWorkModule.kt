6868f02f648858cd72d9ae7843951bb347aaa3e5
Maldeojos <tiansanchez2000@gmail.com>
2026-06-22 09:58:56 -0400

    xd

--- app/src/main/java/com/develop/myapplication/di/NetWorkModule.kt
+++ app/src/main/java/com/develop/myapplication/di/NetWorkModule.kt
@@ -1,4 +1,27 @@
 package com.develop.myapplication.di
 
-class NetworkModule {
+import com.develop.myapplication.data.remote.service.HospitalApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    object NetworkModule {
+
+        @Provides
+        @Singleton
+        fun provideRetrofit(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl("http://192.168.0.2:8000/api/")
+                .baseUrl("http://10.0.2.2:8000/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
 }
