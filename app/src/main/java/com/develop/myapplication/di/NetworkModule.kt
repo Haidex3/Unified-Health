36349ed458b8c402f508fab0be32d2f24793e6a9
Maldeojos <tiansanchez2000@gmail.com>
2026-06-22 09:55:57 -0400

    xd

--- app/src/main/java/com/develop/myapplication/di/NetworkModule.kt
+++ app/src/main/java/com/develop/myapplication/di/NetworkModule.kt
@@ -15,31 +15,34 @@ import retrofit2.converter.gson.GsonConverterFactory
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+    object NetworkModule {
 
-    @Provides
-    @Singleton
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8000/api/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+        @Provides
+        @Singleton
+        fun provideRetrofit(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl("http://192.168.0.2:8000/api/")
+                .baseUrl("http://10.0.2.2:8000/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
 
-    @Provides
-    @Singleton
-    fun provideHospitalApiService(retrofit: Retrofit): HospitalApiService {
-        return retrofit.create(HospitalApiService::class.java)
-    }
+        @Provides
+        @Singleton
+        fun provideHospitalApiService(retrofit: Retrofit): HospitalApiService {
+            return retrofit.create(HospitalApiService::class.java)
+        }
 
-    @Provides
-    @Singleton
-    fun provideMedicoApiService(retrofit: Retrofit): MedicoApiService {
-        return retrofit.create(MedicoApiService::class.java)
-    }
+        @Provides
+        @Singleton
+        fun provideMedicoApiService(retrofit: Retrofit): MedicoApiService {
+            return retrofit.create(MedicoApiService::class.java)
+        }
 
-    @Provides
-    @Singleton
-    fun providePacienteApiService(retrofit: Retrofit): PacienteApiService {
-        return retrofit.create(PacienteApiService::class.java)
+        @Provides
+        @Singleton
+        fun providePacienteApiService(retrofit: Retrofit): PacienteApiService {
+            return retrofit.create(PacienteApiService::class.java)
+        }
     }
 }
