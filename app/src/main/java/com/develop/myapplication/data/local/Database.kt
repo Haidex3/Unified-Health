a58075cdbf4935a18e23c982f1c837be5c593989
asuredragun <76012177+asuredragun@users.noreply.github.com>
2026-06-25 23:01:28 -0400

    arreglo de medicoviewmodel

--- app/src/main/java/com/develop/myapplication/data/local/Database.kt
+++ app/src/main/java/com/develop/myapplication/data/local/Database.kt
@@ -21,7 +21,6 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var Instance: AppDatabase? = null
         fun getDatabase(context: Context): AppDatabase {
-        // if the Instance is not null, return it, otherwise create a new database instance.
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, AppDatabase::class.java, "App_database")
                     .build().also { Instance = it }
