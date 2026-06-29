fd548d9c7eb2ce65928fc049bf7b1f8990aec044
Maldeojos <tiansanchez2000@gmail.com>
2026-06-29 17:33:20 -0400

    cambios

--- app/src/main/java/com/develop/myapplication/data/remote/service/MedicoApiService.kt
+++ app/src/main/java/com/develop/myapplication/data/remote/service/MedicoApiService.kt
@@ -2,6 +2,7 @@ package com.develop.myapplication.data.remote.service
 
 import com.develop.myapplication.data.remote.dto.MedicoCreateDto
 import com.develop.myapplication.data.remote.dto.MedicoDto
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
